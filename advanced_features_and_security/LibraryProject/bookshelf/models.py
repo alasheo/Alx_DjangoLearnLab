6bb8ffffc009209fd898d25bcd72cf73e4be85c3
alasheo <51996319+alasheo@users.noreply.github.com>
2025-04-14 18:07:59 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -23,6 +23,21 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(username, email, password, **extra_fields)
 
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
+
 # Custom user model
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
