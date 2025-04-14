d8a230e61597b253d50dcd09983a9857776c022b
alasheo <51996319+alasheo@users.noreply.github.com>
2025-04-14 20:56:29 +0100

    Update urls.py

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -1,26 +1,11 @@
-"""
-URL configuration for LibraryProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from bookshelf import views  # Make sure you're importing from the 'bookshelf' app
+from bookshelf import views  # Ensure you're importing from the 'bookshelf' app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-      path('books/', views.book_list, name='book_list'),
+    path('books/', views.book_list, name='book_list'),
     path('books/create/', views.create_book, name='create_book'),
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('example/', views.example_form_view, name='example_form'),  # Add this line for the example form
 ]
