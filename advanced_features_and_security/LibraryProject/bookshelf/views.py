1064630ac2400d86db669e71aef0e9e9bdcfc80d
alasheo <51996319+alasheo@users.noreply.github.com>
2025-04-14 20:52:34 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from django.views.decorators.csrf import csrf_protect
 from .models import Book
-from django import forms
-from .forms import BookForm
+from .forms import BookForm, ExampleForm  # Import the ExampleForm
 
 # --- Form class to validate inputs and avoid raw request.POST usage ---
 class BookForm(forms.ModelForm):
@@ -11,13 +10,11 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ['title', 'author', 'publication_year']
 
-
 # Book List View
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()  # Safe ORM query
     return render(request, 'bookshelf/book_list.html', {'books': books})
-
 
 # Book Create View
 @permission_required('bookshelf.can_create', raise_exception=True)
@@ -32,7 +29,6 @@ def create_book(request):
         form = BookForm()
     return render(request, 'bookshelf/create_book.html', {'form': form})
 
-
 # Book Delete View
 @permission_required('bookshelf.can_delete', raise_exception=True)
 @csrf_protect
@@ -42,3 +38,14 @@ def delete_book(request, book_id):
         book.delete()
         return redirect('book_list')  # Redirect to book list after deletion
     return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+# Example Form View (for contact or feedback)
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the data here (e.g., save to database or send email)
+            return redirect('thank_you')  # Redirect after successful form submission
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/example_form.html', {'form': form})
