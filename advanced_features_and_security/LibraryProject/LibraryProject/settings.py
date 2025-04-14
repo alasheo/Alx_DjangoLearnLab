ce769b5abe97048cee53bf974d238bfb7f44f938
alasheo <51996319+alasheo@users.noreply.github.com>
2025-04-14 17:45:28 +0100

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -126,5 +126,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
